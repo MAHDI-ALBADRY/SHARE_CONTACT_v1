55b04f68418112326b1543edae594d94376814c6
Mahdi ALBadry <32489976+MAHDI-ALBADRY@users.noreply.github.com>
2018-02-16 19:32:34 +0300

    Update JHAT_@MAHDI_ALBADRY.php

--- JHAT_@MAHDI_ALBADRY.php
+++ JHAT_@MAHDI_ALBADRY.php
@@ -7,10 +7,10 @@ $first_name = '|جهه 1|';
 $phone_number1 = '+13155025228'; // رقم المطور2 
 $first_name1 = '|جهه 2|';
 $channel = "@L_CC_L"; // قناة اولا
-$channel1 = "@YYIIPP"; // قناة ثانيه
+$channel1 = "@DEV_WEBsite"; // قناة ثانيه
 $sudo = 93252375; // مطور البوت
 ob_start();
-$API_KEY = "538231601:AAHFX2HXwXewpngJoxBwB8hPgcB8_OYW7x0"; // توكنـــك هنا
+$API_KEY = "TOKEN"; // توكنـــك هنا
 echo "https://api.telegram.org/bot$API_KEY/setwebhook?url=".$_SERVER['SERVER_NAME']."".$_SERVER['SCRIPT_NAME'];
 define('API_KEY',$API_KEY);
   //SHARE Contact  V1
